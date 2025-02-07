a3ac795ed03900ce96004bb102e1a487c7816f88
unknown <ashikahmedrabbi@gmail.com>
2025-02-08 02:41:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function logFormSubmission() {
+    console.log('Contact form submitted');
+    alert('Thank you for contacting us! We will get back to you shortly.');
+  }
+  
+  function logPolicyView(policy) {
+    console.log('Viewed ' + policy);
+  }
